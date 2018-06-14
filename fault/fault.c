43c4ae1e3446f5c09e1eaad72bd2a3195de2cd0f
c.tedie <c.tedie@comeca-group.com>
2018-06-14 12:01:02 +0200

    [FAULT]  * Mise en place de la structure du module

--- fault/fault.c
+++ fault/fault.c
@@ -21,6 +21,7 @@
 /* Includes --------------------------------------------------------------------------------------------------------*/
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 #include "fault.h"
 
@@ -30,7 +31,132 @@
 /* Public variables ------------------------------------------------------------------------------------------------*/
 /* Private variables -----------------------------------------------------------------------------------------------*/
 /* Private functions prototypes ------------------------------------------------------------------------------------*/
+static void fctState_Inactive(void *pArg);
+static void fctState_Activating(void *pArg);
+static void fctState_Active(void *pArg);
+static void fctState_Inactivating(void *pArg);
+
 /* Private functions -----------------------------------------------------------------------------------------------*/
+
+/**
+ *********************************************************
+ * \brief
+ *
+ * \param [in]
+ * \param [out]
+ *
+ * \return
+ *********************************************************/
+static void fctState_Inactive(void *pArg)
+{
+    fault_t *pFault = (fault_t*)pArg;
+
+    if((pFault->appearenceCond != NULL) && (pFault->appearenceCond(NULL) == true))
+    {
+        //Detected Fault
+        pFault->state = ACTIVE;
+        pFault->fctState = fctState_Active;
+
+        if(pFault->appearenceAction != NULL)
+        {
+            pFault->appearenceAction(NULL);
+        }
+
+        //TODO Update fault mask
+        switch (pFault->faultType) {
+            case INFO:
+                FAULT_InfoMask |= (1 << pFault->id);
+                break;
+            case MINOR:
+                FAULT_MinorMask |= (1 << pFault->id);
+                break;
+            case MAJOR:
+                FAULT_MajorMask |= (1 << pFault->id);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+/**
+ *********************************************************
+ * \brief
+ *
+ * \param [in]
+ * \param [out]
+ *
+ * \return
+ *********************************************************/
+static void fctState_Active(void *pArg)
+{
+    fault_t *pFault = (fault_t*)pArg;
+    uint64_t mask = 0;
+
+    if((pFault->disappearenceCond != NULL) && (pFault->disappearenceCond(NULL) == true))
+    {
+        //Disappeared Fault
+        pFault->state = ACTIVE;
+        pFault->fctState = fctState_Active;
+
+        if(pFault->disappearenceAction != NULL)
+        {
+            pFault->disappearenceAction(NULL);
+        }
+
+        mask = (1 << pFault->id);
+
+        switch (pFault->faultType)
+        {
+            case INFO:
+                FAULT_InfoMask &= ~mask;
+                break;
+            case MINOR:
+                FAULT_MinorMask &= ~mask;
+                break;
+            case MAJOR:
+                FAULT_MajorMask &= ~mask;
+                break;
+            default:
+                break;
+        }
+    }
+
+}
+
+/**
+ *********************************************************
+ * \brief
+ *
+ * \param [in]
+ * \param [out]
+ *
+ * \return
+ *********************************************************/
+static void fctState_Activating(void *pArg)
+{
+    fault_t *pFault = (fault_t*)pArg;
+
+
+}
+
+/**
+ *********************************************************
+ * \brief
+ *
+ * \param [in]
+ * \param [out]
+ *
+ * \return
+ *********************************************************/
+static void fctState_Inactivating(void *pArg)
+{
+    fault_t *pFault = (fault_t*)pArg;
+
+
+}
+
+
 /* Public functions ------------------------------------------------------------------------------------------------*/
 
 /**
@@ -42,7 +168,64 @@
  *
  * \return
  *********************************************************/
+void FAULT_Init(void)
+{
+    uint32_t indexFault;
+    fault_t *pFault;
 
+    FAULT_InfoMask = 0;
+    FAULT_MinorMask = 0;
+    FAULT_MajorMask = 0;
+
+    for (indexFault = 0; FAULT_tab[indexFault].pFault != NULL; ++indexFault)
+    {
+        pFault = FAULT_tab[indexFault].pFault;
+
+        pFault->state = pFault->initState;
+        switch (pFault->state)
+        {
+            case INACTIVE:
+                pFault->fctState = fctState_Inactive;
+                break;
+            case ACTIVATING:
+                pFault->fctState = fctState_Activating;
+                break;
+            case ACTIVE:
+                pFault->fctState = fctState_Active;
+                break;
+            case INACTIVATING:
+                pFault->fctState = fctState_Inactivating;
+                break;
+        }
+
+        pFault->id = indexFault;
+
+    }
+
+}
+
+/**
+ *********************************************************
+ * \brief
+ *
+ * \param [in]
+ * \param [out]
+ *
+ * \return
+ *********************************************************/
+void FAULT_Process(void)
+{
+    uint16_t faultId;
+
+    fault_t *pFault = NULL;
+
+    for (faultId = 0; FAULT_tab[faultId].pFault != NULL; ++faultId)
+    {
+        pFault = FAULT_tab[faultId].pFault;
+
+        pFault->fctState(pFault);
+    }
+}
 
 /** \} */
 /******************************************************** EOF *******************************************************/
--- fault/fault.h
+++ fault/fault.h
@@ -32,12 +32,61 @@ extern "C"
 #endif
 
 /* Includes --------------------------------------------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
 
 /* Public Macro definition -----------------------------------------------------------------------------------------*/
 /* Public Constant definition --------------------------------------------------------------------------------------*/
+#define FAULT_MAX_NB_FAULT  50
+
 /* Public Type definition  -----------------------------------------------------------------------------------------*/
+typedef enum
+{
+    INACTIVE = 0,
+    ACTIVATING,
+    ACTIVE,
+    INACTIVATING
+}faultState_t;
+
+typedef enum
+{
+    INFO = 0,
+    MINOR,
+    MAJOR
+}faultType_t;
+
+typedef struct
+{
+    bool (*appearenceCond)(void* pArg);
+    bool (*disappearenceCond)(void* pArg);
+    void (*appearenceAction)(void* pArg);
+    void (*disappearenceAction)(void* pArg);
+    void (*activeAction)(void* pArg);
+
+    faultType_t faultType;
+
+    faultState_t initState;
+    faultState_t state;
+    uint64_t id;
+    void (*fctState)(void *pArg);
+
+}fault_t;
+
+typedef struct
+{
+    fault_t *pFault;
+}faultDesc_t;
+
 /* Public variables ------------------------------------------------------------------------------------------------*/
+extern uint64_t FAULT_InfoMask;
+extern uint64_t FAULT_MinorMask;
+extern uint64_t FAULT_MajorMask;
+
+extern const faultDesc_t FAULT_tab[FAULT_MAX_NB_FAULT];
+
 /* Public functions ------------------------------------------------------------------------------------------------*/
+void FAULT_Init(void);
+void FAULT_Process(void);
 
 #endif /* FAULT_H_ */
 
