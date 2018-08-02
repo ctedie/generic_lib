282f0486279c398c5b44b191573efcb8c474fc17
c.tedie <c.tedie@comeca-group.com>
2018-08-02 17:16:40 +0200

    [PTIME]  * Correction du calcul des dates

--- time/ptime.c
+++ time/ptime.c
@@ -31,10 +31,14 @@
 /* Constant definition ---------------------------------------------------------------------------------------------*/
 //#define YEAR_IN_SECOND  31556926U
 
-#define YEAR_IN_SECOND  31557600U //365.25
+#define SECOND_IN_DAY       86400U
+#define SECOND_IN_MINUTE    60UL
+#define SECOND_IN_HOUR      3600UL
+#define SECOND_IN_YEAR  (365UL * 86400UL) //365days
+#define SECOND_IN_LEAP_YEAR  (366UL * 86400UL) //366days
+#define SECOND_IN_YEAR_JULLIAN  (31557600UL) //365.25days
+
 // End of year 1970     31535999
-//#define YEAR_IN_SECOND  31556926U //365.24
-#define DAY_IN_SECOND   86400U
 
 uint16_t m_isLeapYear = 0;
 static uint16_t TAB_MONTH[] =
@@ -116,57 +120,66 @@ inline uint64_t PTIME_get(void)
  *
  * \return
  *********************************************************/
-void PTIME_getDate(ptime_t *date)
+void PTIME_getDate(ptime_t *pDate)
 {
     uint64_t s = PTIME_timestamp_ms / 1000;
     uint64_t years = s;
     uint16_t days;
     uint16_t i;
+    uint64_t  nbSecToSubtract;
+    bool bContinue = true;
 
 
-    date->year = 1970 + (years / YEAR_IN_SECOND);
-    years = years % YEAR_IN_SECOND;
-
-    date->__dayInYear = (years / DAY_IN_SECOND)+1;
-    days = (years / DAY_IN_SECOND) + 1;
-    years = years % DAY_IN_SECOND;
-
-    date->hours = (s / 3600) % 24;
-    s = s % 3600;
-
-    date->minutes = s / 60;
-    s = s % 60;
-
-    date->seconds = s;
-
-    if(days > 57)
+    pDate->year = 1970;
+    while(bContinue)
     {
-        if(((date->year % 4 == 0) && (date->year % 100 != 0))
-           ||((date->year % 400 == 0) && (date->year % 100 == 0)))
+        nbSecToSubtract = (IS_LEAP(pDate->year)) ? SECOND_IN_LEAP_YEAR :SECOND_IN_YEAR;
+        if(s >= nbSecToSubtract)
         {
-            //Année bissextile
-            TAB_MONTH[1] = 29;
+            s -= nbSecToSubtract;
+            pDate->year++;
         }
         else
         {
-            TAB_MONTH[1] = 28;
-        }
-    }
-    //calc day and month
-    for ( i = 0; i < 12; ++i)
-    {
-        if(days > TAB_MONTH[i])
-        {
-            days -= TAB_MONTH[i];
-        }
-        else
-        {
-            date->month = i + 1;
-            date->day = days;
-            break;
+            bContinue = false;
         }
     }
 
+    pDate->month = 1;
+    bContinue = true;
+    while(bContinue)
+    {
+        if((pDate->month == 2)&&(IS_LEAP(pDate->year)))
+        {
+            nbSecToSubtract = 29UL*SECOND_IN_DAY;
+        }
+        else
+        {
+            nbSecToSubtract = TAB_MONTH[pDate->month - 1] * SECOND_IN_DAY;
+        }
+
+        if(s >= nbSecToSubtract)
+        {
+            s -= nbSecToSubtract;
+            pDate->month++;
+        }
+        else
+        {
+            bContinue = false;
+        }
+    }
+
+    pDate->day = (uint16_t)(s/SECOND_IN_DAY);
+    s -= ((uint64_t)pDate->day * SECOND_IN_DAY);
+    pDate->day+=1;
+
+    pDate->hours = (uint16_t)(s/SECOND_IN_HOUR);
+    s -= ((uint64_t)pDate->hours * SECOND_IN_HOUR);
+
+    pDate->minutes = (uint16_t)(s/SECOND_IN_MINUTE);
+    s -= ((uint64_t)pDate->minutes * SECOND_IN_MINUTE);
+
+    pDate->seconds = (uint16_t)s;
 }
 
 /**
@@ -181,32 +194,42 @@ void PTIME_getDate(ptime_t *date)
 void PTIME_setDate(ptime_t *date)
 {
     uint16_t i;
-    uint16_t days = 0;
+    bool isLeap;
 
-    PTIME_timestamp_ms = (uint64_t)((uint64_t)(date->year - 1970) * YEAR_IN_SECOND) * 1000;
+    PTIME_timestamp_ms = 0;
+    isLeap = IS_LEAP(date->year);
 
-    //check leap year
-    if(IS_LEAP(date->year))
+    for (i = 1970; i < date->year; ++i)
     {
-        //Année bissextile
-        TAB_MONTH[1] = 29;
+        if(IS_LEAP(i))
+        {
+            PTIME_timestamp_ms += SECOND_IN_LEAP_YEAR;
+        }
+        else
+        {
+            PTIME_timestamp_ms += SECOND_IN_YEAR;
+        }
     }
-    else
-    {
-        TAB_MONTH[1] = 28;
-    }
-
 
     for (i = 1; i < date->month; ++i)
     {
-        days += TAB_MONTH[i-1];
+        if((i==2)&(isLeap))
+        {
+            PTIME_timestamp_ms += 29UL * SECOND_IN_DAY;
+        }
+        else
+        {
+            PTIME_timestamp_ms += (uint64_t)TAB_MONTH[i-1] * SECOND_IN_DAY;
+        }
     }
 
-    PTIME_timestamp_ms += (uint64_t)((uint64_t)(days + (date->day - 1)) * 86400) * 1000;
-    PTIME_timestamp_ms += (uint64_t)date->hours * 3600 * 1000;
-    PTIME_timestamp_ms += (uint64_t)date->minutes * 60 * 1000;
-    PTIME_timestamp_ms += (uint64_t)date->seconds * 1000;
 
+    PTIME_timestamp_ms += (uint64_t)(date->day - 1) * SECOND_IN_DAY;
+    PTIME_timestamp_ms += (uint64_t)date->hours * SECOND_IN_HOUR;
+    PTIME_timestamp_ms += (uint64_t)date->minutes * SECOND_IN_MINUTE;
+    PTIME_timestamp_ms += (uint64_t)date->seconds;
+
+    PTIME_timestamp_ms = PTIME_timestamp_ms * 1000;
 
 
 }
